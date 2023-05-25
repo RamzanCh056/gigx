b8ee3403237c5c03bcd89cae0ce48b3f6b6bde51
RamzanCh056 <79650406+RamzanCh056@users.noreply.github.com>
2023-05-25 21:32:10 +0500

    Push

--- lib/screen/splash_screens.dart
+++ lib/screen/splash_screens.dart
@@ -47,6 +47,8 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
+
+
     Timer(const Duration(seconds: 1), () {
       checkLoginStatus();
     });
